6e65cdea3300e91b8545b4dc5d275ae4b9e417f5
Leonardo Cunha <leonardocunha.a@gmail.com>
2023-01-30 13:49:09 -0300

    aula basica sobre operadores + exercicios

---
+++ python_basico/aula04_exercicio_IMC.py
@@ -0,0 +1,10 @@
+nome = 'Leonardo'
+peso = 82.0
+altura = 1.73
+
+imc = peso / (altura ** 2)
+
+print (nome, 'tem', altura, 'de altura')
+print ('pesa', peso, 'quilos e seu IMC é:', imc)
+
+... #ellipsis, significa que vc vai colocar algo ali porem não pensou sobre ainda
