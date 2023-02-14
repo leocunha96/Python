2022dd09e01e1439bcc42fdcbd0f669f7d44da58
Leonardo <leonardo@leonardo.myguest.virtualbox.org>
2023-02-13 21:49:12 -0300

    introdução a funções e exercicio 1

---
+++ intermediario/funcoes/aula01_introducao.py
@@ -0,0 +1,9 @@
+def somatorio (num1, num2) :
+    num1 = int(num1)
+    num2 = int (num2)
+    resultado = num1 + num2
+    print (resultado)
+
+primeiroNumero = input ('Digite o primeiro numero: ')
+segundoNumero = input ('Digite o segundo numero: ')
+somatorio (primeiroNumero, segundoNumero)
---
+++ intermediario/funcoes/exercicio1.py
@@ -0,0 +1,37 @@
+'''
+crie uma função que multiple todos os argumentos nao nomeados recebidos.
+Retorne o total para uma variável e mostre a variavel com o resultado.
+
+Crie outra função que diga que o numero é par ou impar, e retorne par ou impar.
+
+'''
+
+
+def produto (*args) :
+    
+    lista = list(args)
+
+    if '0' in lista :
+        resultado = 0
+        return resultado
+    
+    else :
+        resultado = 1
+        for numero in lista :
+            resultado *= numero
+        return resultado
+
+
+def parImpar (numero) :
+    if numero % 2 == 0 :
+        return 'Par'
+    else: 
+        return 'Impar'
+
+
+numero = produto (1,2,3,4,5)
+print (numero)
+
+
+tipo = parImpar (numero)
+print (tipo)
